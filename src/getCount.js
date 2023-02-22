ebc2a44375fb29913edffbce861cbf843b9014f6
dogeyboy1932 <gogineni.akhil@hotmail.com>
2023-02-22 11:14:05 -0600

    First Commit

--- src/getCount.js
+++ src/getCount.js
@@ -3,15 +3,17 @@ import abi from "./utils/abi.json"; // you will have to update this with your co
 
 export default function getCount(provider, contractAddress) {
     console.log("Calling get_count()!");
-    // const counterContract = new ethers.Contract(contractAddress, abi, provider);
+    const counterContract = new ethers.Contract(contractAddress, abi, provider);
+
+    
 
     // WE NOW HAVE A CONTRACT OBJECT ... we just have to call the method you defined in your contract to get the count!
 
-    //  const result = counterContract.getCount() . . . something like this
+    const result = counterContract.getCount();
 
     //  Also beware that result may be a big number, not an int!
 
     // https://docs.ethers.org/v5/api/contract/contract/#Contract--methods
-    return 0;
+    return result;
 
 }
